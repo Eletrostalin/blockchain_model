9f8ab818b324d87e2727e44e304e5fc60b45551b
nickstanchenkov <stanchenkovnikita@gmail.com>
2024-09-30 15:01:03 -0400

    Модель блокчейна на fastapi

--- app/app.py
+++ app/app.py
@@ -1,38 +1,34 @@
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 import logging
-from flask import Flask, render_template, redirect, url_for, request
-from block import create_block, check_integrity
+from .block import create_block, check_integrity
 
-app = Flask(__name__)
+app = FastAPI()
 
 # Настроим логирование
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
+templates = Jinja2Templates(directory="templates")
 
-@app.route('/', methods=['POST', 'GET'])
-def index():
+
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
     logging.info("Запрос на главную страницу")
-
-    if request.method == 'POST':
-        lender = request.form['lender']
-        amount = request.form['amount']
-        borrower = request.form['borrower']
-
-        logging.info(f"POST-запрос: lender={lender}, amount={amount}, borrower={borrower}")
-        create_block(name=lender, amount=amount, to_whom=borrower)
-
-        logging.info("Блок успешно создан")
-        return redirect(url_for('index'))
-
-    return render_template('index.html')
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
-@app.route('/cheking', methods=["GET"])
-def check():
+@app.post("/", response_class=HTMLResponse)
+async def create_new_block(request: Request, lender: str = Form(...), amount: str = Form(...), borrower: str = Form(...)):
+    logging.info(f"POST-запрос: lender={lender}, amount={amount}, borrower={borrower}")
+    create_block(name=lender, amount=amount, to_whom=borrower)
+    logging.info("Блок успешно создан")
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.get("/checking", response_class=HTMLResponse)
+async def check(request: Request):
     logging.info("Запрос на проверку целостности блоков")
     results = check_integrity()
     logging.info(f"Результаты проверки: {results}")
-    return render_template('index.html', results=results)
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    return templates.TemplateResponse('index.html', {"request": request, "results": results})
